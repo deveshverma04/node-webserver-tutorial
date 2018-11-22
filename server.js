c406b78595860d67d4449cdf50e202f92be88d30
Devesh Verma <deveshverma@hcl.com>
2018-11-22 19:02:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var timeStamp = new Date().toString();
+    var log = `${timeStamp} ${req.method} ${req.path} ${req.host}`;
+    console.log(log);
+    fs.appendFile('log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append file.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maint.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Welcome',
+        message: 'Welcome to my abode'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Us'
+    });
+});
+
+app.listen(3000);
